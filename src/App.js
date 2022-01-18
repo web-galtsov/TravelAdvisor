088897720d8eec1210f8a4c4c5f39fae4156db5a
web-galtsov <oksana.galtsov@gmail.com>
2022-01-18 11:41:18 +0200

    start project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import { CssBaseline, Grid, ThemeProvider,createMuiTheme } from '@material-ui/core';
+import Header from "./componenets/Header/Header";
+import List from "./componenets/List/List";
+import Map from "./componenets/Map/Map";
+import { getPlacesData } from "./api/index";
+const theme = createMuiTheme({
+    palette: {
+        type: "dark",
+        primary: {
+            main: "#00AFCE",
+            color: "#fff",
+        },
+        secondary: {
+            main: "#0081A0"
+        }
+    },
+});
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+ const App = () => {
+     const [places, setPlaces ] = useState([]);
+     const [filteredPlaces, setFilteredPlaces] = useState([]);
+     const [childClicked, setChildClicked] = useState(null)
+     const [coordinates, setCoordinates] = useState({})
+     const [bounds, setBounds] = useState({});
+     const [isLoading, setIsLoading] = useState(false);
+     const [type, setType] = useState("restaurants");
+     const [rating, setRating] = useState("");
 
-export default App;
+     useEffect(() => {
+       navigator.geolocation.getCurrentPosition(({
+           coords: { latitude, longitude}}) => {
+           setCoordinates({lat: latitude, lng: longitude});
+       })
+},[]);
+
+     useEffect(() => {
+         const filteredPlaces = places.filter((place) => place.rating > rating);
+         setFilteredPlaces(filteredPlaces);
+     },[rating]);
+
+     useEffect(() => {
+         if(bounds.sw && bounds.ne) {
+             setIsLoading(true);
+
+             getPlacesData(type, bounds.sw, bounds.ne)
+                 .then((data) => {
+                     setPlaces(data?.filter((place) => place.name && place.num_reviews > 0));
+                     setFilteredPlaces([]);
+                     setIsLoading(false);
+                 })
+         }
+     },[type,bounds]);
+
+
+     return (
+         <ThemeProvider theme={theme}>
+
+         <CssBaseline/>
+               <Header setCoordinates={setCoordinates}/>
+            <Grid container spacing={3} style={{width: "100%"}}>
+                    <Grid item  xs={12} md={4}>
+                        <List
+                            places={filteredPlaces.length ? filteredPlaces : places}
+                            childClicked={childClicked}
+                            isLoading={isLoading}
+                            type={type}
+                            setType={setType}
+                            rating={rating}
+                            setRating={setRating}
+                        />
+                    </Grid>
+                    <Grid item xs={12} md={8}>
+                        <Map
+                        setCoordinates={setCoordinates}
+                        setBounds={setBounds}
+                        coordinates={coordinates}
+                        places={filteredPlaces.length ? filteredPlaces : places}
+                        setChildClicked={setChildClicked}
+                        />
+                    </Grid>
+            </Grid>
+         </ThemeProvider>
+     )
+ }
+ export default App;
---
+++ src/api/index.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+export const getPlacesData = async (type, sw, ne) => {
+    try {
+        const { data : { data }} = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`, {
+            params: {
+                bl_latitude: sw.lat ,
+                tr_latitude: ne.lat,
+                bl_longitude: sw.lng ,
+                tr_longitude: ne.lng,
+            },
+            headers: {
+                'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+                'x-rapidapi-key': '71925154c6msh7d2c13ba5a6c63ap131a47jsn1d4d258f672c'
+            }
+        });
+        return data;
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+
+
+
+
